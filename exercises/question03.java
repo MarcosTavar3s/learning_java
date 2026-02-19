b047b6357c64924725a7dfbfb82952d7c0eeb2b6
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2026-02-19 10:34:47 -0300

    Create question03.java

---
+++ exercises/question03.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+// calculates money spent on cigarretes
+public class question03{
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type the price of a cigarrete: ");
+        float price = reader.nextFloat(); 
+        System.out.print("Type how many years you have smoked: ");
+        int years = reader.nextInt();
+        System.out.print("Type how many cigarretes you have smoked daily: ");
+        int cigarretesPerDay = reader.nextInt();
+        
+        // supposing that there aren't leap years
+        float cost = 365 * cigarretesPerDay * years * price;
+        
+        System.out.println("Money spent on cigarretes: R$" + String.format("%.2f", cost));
+        }
+}
