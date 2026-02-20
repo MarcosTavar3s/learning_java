dadde0116853964996f765f6ba21e74da8bf4f86
Marcos Aurélio Tavares Filho <86982304+MarcosTavar3s@users.noreply.github.com>
2026-02-20 07:59:06 -0300

    Create question08.java

---
+++ exercises/question08.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+// calculates a arithmetic progression
+public class question08{
+	public static void main(String[] args) {
+	    float common_difference, first_n, last_n, n;
+	   // int n;
+	    
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Common difference: ");
+        common_difference = reader.nextFloat();
+        System.out.println("First number: ");
+        first_n = reader.nextFloat();
+        System.out.println("Last number: ");
+        last_n = reader.nextFloat();
+
+        n = (last_n - first_n) / common_difference + 1;
+        
+        System.out.println("Arithmetic progression sum: " + (n/2 * (first_n + last_n)));
+	}
+}
